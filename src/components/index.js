358ac1c1d1fad72abf68a658c20bdc300b7ecaaa
subhasis020299 <subhasisdas125@gmail.com>
2021-03-26 11:48:46 +0530

    search implemented

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import SideNav from "./SideNav";
-import Header from "./Header";
 import Loader from "./Loader";
 import ImageCard from "./ImageCard";
 
-export { SideNav, Header, ImageCard, Loader };
+export { SideNav, ImageCard, Loader };
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { Header, ImageCard, Loader } from "../components";
+import React, { useState, useEffect, useCallback } from "react";
+import { Container, Row, Col, Form, Button, InputGroup } from "react-bootstrap";
+import { ImageCard, Loader } from "../components";
 import api from "../utils/api";
 
 const Home = () => {
-  const [state, setState] = useState([]);
+  const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [showClear, setShowClear] = useState(false);
 
-  useEffect(
+  const listPhotos = useCallback(
     () =>
       (async () => {
         setLoading(true);
         try {
           const { response } = await api.photos.list({ page: 1, perPage: 6 });
-          setState((state) => [...state, ...response.results]);
+          setPhotos(response.results);
         } catch (error) {
           console.error(error);
         }
@@ -21,17 +24,91 @@ const Home = () => {
       })(),
     []
   );
-  console.log(state);
+
+  useEffect(() => listPhotos(), [listPhotos]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    if (value === "" || value.length === 0) {
+      setShowClear(false);
+    } else {
+      setShowClear(true);
+      setSearchText(e.target.value);
+    }
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    setSearchText("");
+    setShowClear(false);
+    if (isSearching) {
+      setIsSearching(false);
+      listPhotos();
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (searchText.length > 0) {
+      setLoading(true);
+      setIsSearching(true);
+      try {
+        const { response } = await api.search.getPhotos({
+          query: searchText,
+          page: 1,
+          perPage: 6,
+        });
+
+        setPhotos(response.results);
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    } else {
+      alert("Please enter something to search!");
+    }
+  };
 
   return (
     <Container>
-      <Header page="home" />
+      <h1 className="text-left">Home</h1>
+      <small style={{ color: "#9999A2" }}>Photos for everyone...</small>
+      <Form className="my-4" onSubmit={handleSubmit}>
+        <Row>
+          <Col lg="8">
+            <InputGroup>
+              <Form.Control
+                type="text"
+                placeholder="Search..."
+                className="search-bar"
+                value={searchText}
+                onChange={handleChange}
+              />
+              {showClear && (
+                <InputGroup.Append>
+                  <button className="clear-btn" onClick={handleClear}>
+                    Clear
+                  </button>
+                </InputGroup.Append>
+              )}
+            </InputGroup>
+          </Col>
+          <Col lg="4">
+            <Button variant="danger" type="submit">
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
       <hr className="divider" />
+      {isSearching && <span>Showing results for '{searchText}'</span>}
+
       <Row className="image-area">
         {loading ? (
           <Loader />
         ) : (
-          state.map((photo) => (
+          photos.map((photo) => (
             <Col sm="4" key={photo.id}>
               <ImageCard url={photo.urls?.regular} />
             </Col>
